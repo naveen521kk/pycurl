a269a18163dd2ae8e65323c0a71a10993aceae0f
Naveen M K <naveen521kk@gmail.com>
2021-07-09 23:21:53 +0530

    fix

--- winbuild.py
+++ winbuild.py
@@ -255,7 +255,7 @@ def get_nuget_args(bitness, version):
         "-FallbackSource",
         "https://api.nuget.org/v3/index.json",
         "-OutputDirectory",
-        config.python_path_template% dict(bitness=bitness,python_release=version.replace('.','')),
+        install_dir,
     ]
 
 def download_pythons(config):
